f0fa5b3e65bf2a5806c5d713896db05fbca946c3
Sayareyhan <reyhanarya@students.amikom.ac.id>
2023-03-21 13:06:34 +0700

    taxi1

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿namespace TaxiOBjek
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            taxio taxio = new taxio();
+
+            taxio.DriverName = "Supri";
+            taxio.onDuty = false; taxio.onDuty = true;
+            taxio.numPassanger = 69696969;
+
+
+            taxio.info();
+            taxio.naik();
+            taxio.turun();
+
+            Console.ReadKey();
+        }
+    }
+}
